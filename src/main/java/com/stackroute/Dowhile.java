24945fa0be088b3f0d668771030bbc650c271232
keerthika <atmakurikeerthika@gmail.com>
2019-08-03 11:25:43 +0530

    adding do while loop program

---
+++ src/main/java/com/stackroute/Dowhile.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+
+import java.util.Scanner;
+
+public class Dowhile {
+  public static void main(String args[]) {
+    int factorial = 1;int i=1;
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+    do {
+
+      factorial = factorial * i;
+      i++;
+    } while (i<=input);
+    System.out.println(factorial);
+  }
+}
